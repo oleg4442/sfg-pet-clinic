77fb5671ce3d26cb30c4b4a5722aff83b0b70a77
Oleg <o.s.naydin@gmail.com>
2020-01-07 14:10:35 +0100

    broken links fixed. Closes #45

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetController.java
@@ -14,7 +14,7 @@ public class VetController {
         this.verService = verService;
     }
 
-    @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
+    @RequestMapping({"/vets", "/vets/index", "/vets/index.html", "/vets.html"})
     public String listVets(Model model) {
 
         model.addAttribute("vets", verService.findAll());
